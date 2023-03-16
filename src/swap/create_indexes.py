bbf4533287ce9347fe6ab188fc2e2e1af1cbb941
Prince Arora <77785932+princearoragithub@users.noreply.github.com>
2023-03-17 02:58:35 +0530

    moar indexes

--- src/swap/create_indexes.py
+++ src/swap/create_indexes.py
@@ -9,6 +9,7 @@ db_name = indexer_id.replace("-", "_")
 db = mongo[db_name]
 
 db["tokens"].create_index([("id", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["tokens"].create_index([("id", ASCENDING), ("_chain.valid_to", ASCENDING), ("_chain.valid_from", ASCENDING)])
 db["tokens"].create_index([("_chain.valid_to", ASCENDING)])
 db["tokens"].create_index([("_chain.valid_from", ASCENDING)])
 
@@ -30,6 +31,8 @@ db["exchange_day_data"].create_index([("_chain.valid_to", ASCENDING)])
 db["exchange_day_data"].create_index([("_chain.valid_from", ASCENDING)])
 
 db["factories"].create_index([("id", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["factories"].create_index([("_chain.valid_to", ASCENDING), ("_chain.valid_from", ASCENDING)])
+db["factories"].create_index([("_chain.valid_from", ASCENDING)])
 
 db["pair_day_data"].create_index([("pair_id", ASCENDING), ("day_id", ASCENDING), ("_chain.valid_to", ASCENDING)])
 db["pair_day_data"].create_index([("_chain.valid_to", ASCENDING)])
@@ -40,9 +43,25 @@ db["pair_hour_data"].create_index([("_chain.valid_to", ASCENDING)])
 db["pair_hour_data"].create_index([("_chain.valid_from", ASCENDING)])
 
 db["users"].create_index([("id", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["users"].create_index([("_chain.valid_from", ASCENDING)])
 
 db["liquidity_position_snapshots"].create_index([("block", ASCENDING)])
 
 db["transactions"].create_index([("hash", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["transactions"].create_index([("_chain.valid_to", ASCENDING)])
 
 db["swaps"].create_index([("to", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["swaps"].create_index([("transaction_hash", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["swaps"].create_index([("pair_id", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["swaps"].create_index([("pair_id", ASCENDING), ("transaction_hash", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["swaps"].create_index([("_chain.valid_to", ASCENDING)])
+
+db["mints"].create_index([("transaction_hash", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["mints"].create_index([("pair_id", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["mints"].create_index([("pair_id", ASCENDING), ("transaction_hash", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["mints"].create_index([("pair_id", ASCENDING), ("to", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["mints"].create_index([("_chain.valid_to", ASCENDING)])
+
+db["burns"].create_index([("transaction_hash", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["burns"].create_index([("pair_id", ASCENDING), ("_chain.valid_to", ASCENDING)])
+db["burns"].create_index([("_chain.valid_to", ASCENDING)])
